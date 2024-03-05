f42fe8b7a9dc98dc11405dcfc15c7115d2fe02af
SobakaBarabaka74 <153353273+SobakaBarabaka74@users.noreply.github.com>
2024-03-06 00:28:58 +0700

    Add files via upload

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,11 +1,10 @@
 import { Ui } as Room from 'pixel_combats/room'
-import { Main } as PEnter from './Main.js'
+import * as PEnter from './Main.js'
 
 try {
     PEnter.Main({
         version: '0.1'
     })
 } catch(err) {
-    Room.Ui.GetContext().Hint.Value = 
-        'Room error: ' + err.name + '\nDescription error: ' + err.message
+    Room.Ui.GetContext().Hint.Value = 'Room error: ' + err.name + '\nDescription error: ' + err.message
 }
